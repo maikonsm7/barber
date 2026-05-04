943991fb6961ee0214cefd3c5b3b63a2611a8f90
maikonsm7 <maikonsm7@gmail.com>
2026-05-04 11:38:44 -0400

    create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import banner1 from "@/public/banner-01.png";
 
 export default function Home() {
   return (
-   <>
-   <h1>Hello World!</h1>
-   <Button>Click me!</Button>
-   </>
+    <>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Maikon!</h2>
+        <p>Segunda-feira, 4 de Maio.</p>
+
+        <div className="flex mt-5 items-center gap-1">
+        <Input placeholder="Buscar..." />
+        <Button className="ml-2"><Search /></Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-5 rounded-2xl overflow-hidden">
+          <Image src={banner1} alt="Banner 1" fill className="object-cover"/>
+        </div>
+
+      </div>
+    </>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+export default function Header() {
+  return (
+    <Card className="rounded-none py-3">
+      <CardContent className="flex flex-row items-center justify-between">
+        <Image src={logo} alt="Logo" width={120} height={40} />
+        <Button variant="outline" size="icon"><Menu /></Button>
+      </CardContent>
+    </Card>
+  );
+}
