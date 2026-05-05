263f9667be99bc2529c1584670779b400b30e897
maikonsm7 <maikonsm7@gmail.com>
2026-05-04 21:23:52 -0400

    add: menu

--- components/header.tsx
+++ components/header.tsx
@@ -3,12 +3,25 @@ import Image from "next/image";
 import logo from "@/public/logo.png";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from "./ui/sheet";
 export default function Header() {
   return (
     <Card className="rounded-none py-3">
       <CardContent className="flex flex-row items-center justify-between">
         <Image src={logo} alt="Logo" width={120} />
-        <Button variant="outline" size="icon"><Menu /></Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon"><Menu /></Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>This action cannot be undone.</SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
       </CardContent>
     </Card>
   );
